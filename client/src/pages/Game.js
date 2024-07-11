d1181dcb87136140341acd3a1b4e9dc53f884a1c
David-Nosach <91391933+David-Nosach@users.noreply.github.com>
2024-07-11 13:42:03 -0700

    Add Game component with dynamic game room ID and GameBoard component

---
+++ client/src/pages/Game.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import GameBoard from "../components/GameBoard";
+
+const Game = () => {
+  const { id } = useParams();
+  return (
+    <div>
+      <h1>Game Room: {id}</h1>
+      <GameBoard gameId={id} />
+    </div>
+  );
+};
+
+export default Game;
