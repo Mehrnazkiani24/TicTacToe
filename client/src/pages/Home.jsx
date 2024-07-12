d3f2b2f74a1602424a80057f11fb82ee7aea033a
David-Nosach <91391933+David-Nosach@users.noreply.github.com>
2024-07-12 13:03:23 -0700

    reconfigured to .jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import Login from "../components/Login";
-import JoinRoom from "../components/JoinRoom";
-import GameBoard from "../components/GameBoard";
-import Register from "../components/Register";
+import Login from "../components/Login.jsx";
+import Register from "../components/Register.jsx";
+import JoinRoom from "../components/JoinRoom.jsx";
+import GameBoard from "../components/GameBoard.jsx";
 
 const Home = () => {
   return (
     <div>
       <h1>Tic Tac Toe</h1>
+      <Register />
       <Login />
       <JoinRoom />
     </div>
