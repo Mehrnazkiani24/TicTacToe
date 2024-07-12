0333980c04530f2857fc4ce9a39d3484f8090f15
David-Nosach <91391933+David-Nosach@users.noreply.github.com>
2024-07-12 15:11:34 -0700

    register, login, and room id working properly

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { register, login } = require("../controllers/authController");
 
+console.log("Register function:", register);
+console.log("Login function:", login);
+
 const router = express.Router();
 
 router.post("/register", register);
