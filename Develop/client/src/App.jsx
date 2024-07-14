cca4f92195c90ea914564318021568961f68fd51
mehrnazkiani24 <mehrnaz03@yahoo.com>
2024-07-14 14:03:57 -0700

    added client connection for sockets

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,10 +1,15 @@
+import { useEffect } from 'react';
 import './App.css';
 import { Outlet } from 'react-router-dom';
+import { connectSocket } from './socket';
 
 // Uncomment import statement below after building queries and mutations
 // import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 function App() {
+  useEffect(() => {
+    connectSocket();
+  }, []);
   return (
     <div className="flex-column justify-center align-center min-100-vh bg-primary">
       <Outlet />
