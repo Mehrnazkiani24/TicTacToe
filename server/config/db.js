afd164e746583bdcf0ebf39108fa61643fc18752
AntCamper <152764140+AntCamper@users.noreply.github.com>
2024-07-18 16:28:09 -0700

    updated file structure

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const uri = process.env.MONGODB_URI || "mongodb+srv://a:<a>@cluster0.j7z8x1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(uri); 
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error('MongoDB Connection Error:', err);
